9e93eed50ee607d8ce93ac090ab89c9fe00e214b
MarekL-MarekL <markandmark3@gmail.com>
2024-02-27 15:22:48 +0100

    first version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+//first attempt
+public class Worker
+{
+    static void Main()
+    {
+        Dictionary<string, int> workerList = new Dictionary<string, int>();    
+
+        Console.WriteLine("Enter worker name: ");
+        string workerName = Console.ReadLine();
+        Console.WriteLine("Enter salary: ");
+        int workerSalary = int.Parse(Console.ReadLine());
+        workerList.Add(workerName, workerSalary);
+
+        
+        foreach (KeyValuePair<string, int> item in workerList) {
+            Console.WriteLine("Worker :{0}, has salary : {1}", item.Key, item.Value);
+        }
+    }
+}
